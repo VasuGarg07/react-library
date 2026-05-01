2b41ece56d6689248eb2164e2e9961cd50c10d7f
Vasu Garg <vasugarg6656@gmail.com>
2026-05-01 12:25:47 +0530

    Request Animation Frame

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react";
 import "./ProgressBar.css";
 
-const ProgressBar = ({ value, length = 400 }) => {
+// TODO: Read requestAnimationFrame
+
+const ProgressBar = ({ length = 400 }) => {
+    const [value, setValue] = useState(1);
+
+    useEffect(() => {
+        let intervalId = setInterval(() => {
+            if (value === 100) {
+                clearInterval(intervalId);
+                return;
+            }
+            setValue(prev => Math.min(prev + 1, 100))
+        }, 100);
+
+        return () => clearInterval(intervalId)
+    }, [])
 
     return (
         <div className="progress-container" style={{ width: length }}>
--- src/demos/ProgressBarDemo.jsx
+++ src/demos/ProgressBarDemo.jsx
@@ -2,38 +2,7 @@ import { useState } from "react";
 import ProgressBar from "../components/ProgressBar"
 
 const ProgressBarDemo = () => {
-    const [progress, setProgress] = useState(50);
-
-    const handleProgress = (value) => {
-        let val = parseInt(value);
-        if (isNaN(val)) {
-            setProgress(1);
-            return;
-        }
-
-        if (val > 100) {
-            setProgress(100);
-        } else if (val < 0) {
-            setProgress(1);
-        } else {
-            setProgress(val)
-        }
-    }
-    return (
-        <div>
-            <input
-                type='number'
-                placeholder='Enter Progress Bar Value'
-                min={1}
-                max={100}
-                step={1}
-                onChange={(e) => handleProgress(e.target.value)}
-                value={progress}
-                style={{ width: "200px", marginBottom: "8px" }}
-            />
-            <ProgressBar value={progress} />
-        </div>
-    )
+    return <ProgressBar />
 }
 
 export default ProgressBarDemo
