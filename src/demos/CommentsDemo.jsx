5150ccd7d15744b716f953380bba5afee5c5d439
Vasu Garg <vasugarg7860@gmail.com>
2026-05-02 11:42:15 +0530

    component reorganization

--- src/demos/CommentsDemo.jsx
+++ src/demos/CommentsDemo.jsx
@@ -1,54 +1,13 @@
-import { useState } from "react";
-import CommentBox from "../components/CommentBox";
-import { useComments } from "../components/CommentsContext";
-import "./CommentsDemo.css";
+import React from 'react'
+import CommentsProvider from '../components/CommentsContext'
+import CommentsPanel from '../components/CommentsPanel'
 
 const CommentsDemo = () => {
-    const [input, setInput] = useState("");
-    const { comments, inputRef, dispatch, parentId, setParentId } = useComments();
-
-    const handleSend = () => {
-        dispatch({
-            type: "add",
-            payload: {
-                comment: input,
-                parentId
-            }
-        });
-        setParentId(null);
-        setInput("");
-    }
-
-    const commentIds = Object.keys(comments).filter(id => !comments[id].parentId);
-
-    return (
-        <div className="container comments-main-container">
-            {commentIds.map((id) => (
-                <CommentBox key={id} id={id} />
-            ))}
-            <span className="spacer" />
-            <div className="comment-input-container">
-                {parentId && (
-                    <span className="reply-to">Replying to: <span style={{ fontWeight: 600 }}>#{parentId}</span></span>
-                )}
-                <input
-                    type="text"
-                    placeholder="Enter Comment"
-                    value={input}
-                    ref={inputRef}
-                    onChange={(e) => setInput(e.target.value)}
-                    onKeyDown={(e) => {
-                        if (e.key === "Enter") {
-                            handleSend();
-                        }
-                    }}
-                />
-                <button onClick={handleSend} disabled={!input}>
-                    Send
-                </button>
-            </div>
-        </div>
-    )
+  return (
+    <CommentsProvider>
+        <CommentsPanel />
+    </CommentsProvider>
+  )
 }
 
 export default CommentsDemo
